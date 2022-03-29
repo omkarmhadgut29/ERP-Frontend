7832df28913c847895062848c28c33576933807e
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2022-03-29 23:29:23 +0530

    Additional Data option added

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -4,32 +4,56 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
+import DataSaverOnIcon from "@mui/icons-material/DataSaverOn";
 
 import EmpTable from "./Employee/EmpTable";
 import AddEmployee from "./Employee/AddEmployee";
+import AdditionalData from "./Employee/AdditionalData";
 
 const Employee = () => {
   const [addEmp, setAddEmp] = React.useState(false);
+  const [additionalBtn, setAdditionalBtn] = React.useState(false);
   return (
     <Box>
       <Box display="flex" justifyContent="space-between">
         <Typography variant="h5" component={"div"}>
           Employee{" "}
         </Typography>
-        <Button
-          variant="contained"
-          color="success"
-          startIcon={<AddIcon />}
-          onClick={() => {
-            setAddEmp(true);
-          }}
-        >
-          Add Employee
-        </Button>
+        <Box>
+          <Button
+            variant="contained"
+            color="success"
+            startIcon={<DataSaverOnIcon />}
+            style={{ marginRight: "10px" }}
+            onClick={() => {
+              setAdditionalBtn(true);
+            }}
+          >
+            Additional Data
+          </Button>
+          <Button
+            variant="contained"
+            color="success"
+            startIcon={<AddIcon />}
+            style={{ marginLeft: "10px" }}
+            onClick={() => {
+              setAddEmp(true);
+            }}
+          >
+            Add Employee
+          </Button>
+        </Box>
       </Box>
+
       <br />
       <EmpTable />
       {addEmp ? <AddEmployee addEmp={addEmp} setAddEmp={setAddEmp} /> : null}
+      {additionalBtn ? (
+        <AdditionalData
+          additionalBtn={additionalBtn}
+          setAdditionalBtn={setAdditionalBtn}
+        />
+      ) : null}
     </Box>
   );
 };
